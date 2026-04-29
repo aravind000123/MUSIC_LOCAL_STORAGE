64c35a9a4a6ce10a7d9b14847a074db05f99d856
aravind <gelamaravind@gmail.com>
2026-04-29 10:26:56 +0530

    added laptop and mobile

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 import os
+from user_agents import parse
 
 app = Flask(__name__)
 
-MUSIC_DIR = r'C:\Users\DELL\Music' 
+MUSIC_DIR_PC = r'C:\Users\DELL'
+MUSIC_DIR_MOBILE = r'storage/emulated'
+
+def get_music_dir():
+    ua = parse(request.headers.get('User-Agent', ''))
+    if ua.is_mobile or ua.is_tablet:
+        return MUSIC_DIR_MOBILE
+    return MUSIC_DIR_PC
 
 @app.route('/')
 def index():
+    MUSIC_DIR = get_music_dir()
     songs = []
     for root, dirs, files in os.walk(MUSIC_DIR):
         for file in files:
@@ -17,7 +26,8 @@ def index():
 
 @app.route('/download/<path:filename>')
 def download_file(filename):
+    MUSIC_DIR = get_music_dir()
     return send_from_directory(MUSIC_DIR, filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
